0765a8f3c7d6d308baa683fdd94baa7cf9117adf
El-blank <53506061+El-blank@users.noreply.github.com>
2021-03-07 04:24:48 -0800

    add initial ibrahimweb website files

---
+++ python file/work.py
@@ -0,0 +1,39 @@
+#phrase = "abcdefghijklmnopqrstuvwxyz"
+#print(phrase[0]) 
+#print(phrase[19])
+#print(phrase[20])
+#print(phrase[21])
+#print(phrase[22])
+#print(phrase[23])
+#print(phrase.index("v"))
+#print(phrase.replace("stuv","reel"))
+#print(69)
+#print( 7.5 + 2.5)
+#print ( 36 / (12 + 2))
+#print( 79 % 12)
+#my_num= 5 % 2 
+#print( str(my_num) + " is my password")
+#my_num= -4
+#print(abs(my_num))
+#print(pow (4, 2))
+#print(max(0.01, 0.001))
+#print(min(-0.3, -0.04))
+#print(round(45/34))
+#print(round(4.768))
+#from math import*
+#print(floor(3.2))
+#print(ceil(39.5))
+#my_num= 39.5
+
+#lucky_numbers= [2,4,6,8,10,12,16]
+#friends= ["mike", "messi", "ramos", "pepe", "varane"]
+#friends[2]= "iniesta"
+#friends.append("john")
+#friends.insert(1,"chilwell")
+#friends.remove("messi")
+#lucky_numbers.reverse()
+#coordinates= (4,3)
+#print(coordinates[1])
+character_name= "young"
+print("he is a " + character_name + " boy")
+
